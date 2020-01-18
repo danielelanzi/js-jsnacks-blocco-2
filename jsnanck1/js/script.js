783ca76badc2ed1f4a5fa255155975e013ec46d4
Daniele Lanzi <dnlshiva@gmail.com>
2020-01-18 21:22:27 +0100

    varie

--- jsnanck1/js/script.js
+++ jsnanck1/js/script.js
@@ -2,7 +2,7 @@
 // Inserisci un numero, se è pari stampa il numero, se è dispari
 // stampa il numero successivo
 
-var numero = parseInt(prompt('Inserisci il numero'));
+var numero = parseInt(prompt('Inserisci un numero'));
 console.log('Il primo numero inserito è: ' + numero);
 
 if (numero % 2 == 0) {
---
+++ jsnanck4/js/script.js
@@ -0,0 +1,8 @@
+// <===========================INIZIO========================>
+// Snack4 blocco2
+// Crea due array che hanno un numero di elementi diversi.
+// Aggiungi elementi casuali all’array che ha meno elementi,
+// fino a quando ne avrà tanti quanti l’altro.
+
+
+// <===========================FINE========================>
